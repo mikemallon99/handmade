afb8f65dc8fc0fb43652b04e692d9a07c9a4ceb4
Michael Mallon <mmallon@generallattice.com>
2026-03-27 11:16:05 -0500

    updated build step

--- code/build.bat
+++ code/build.bat
@@ -3,8 +3,8 @@
 set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -DHANDMADE_WIN32=1 -DHANDMADE_SLOW=1 -DHANDMADE_INTERNAL=1 -FC -Z7 
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib Gdi32.lib winmm.lib
 
-IF NOT EXIST ..\_build mkdir ..\_build
-pushd ..\_build
+IF NOT EXIST %~dp0..\_build mkdir %~dp0..\_build
+pushd %~dp0..\_build
 
 REM 32 bit build
 REM cl %CommonCompilerFlags% ..\code\win32_handmade.cpp -Fmwin32_handmade.map /link -subsystem:windows,5.1 %CommonLinkerFlags%
@@ -12,7 +12,7 @@ REM cl %CommonCompilerFlags% ..\code\win32_handmade.cpp -Fmwin32_handmade.map /l
 REM 64 bit build
 del *.pdb > NUL 2> NUL
 echo WAITING FOR PDB > lock.tmp
-cl %CommonCompilerFlags% ..\code\handmade.cpp -Fmhandmade.map -LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameUpdateAndRender -EXPORT:GameGetSoundSamples
+cl %CommonCompilerFlags% %~dp0handmade.cpp -Fmhandmade.map -LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameUpdateAndRender -EXPORT:GameGetSoundSamples
 del lock.tmp
-cl %CommonCompilerFlags% ..\code\win32_handmade.cpp -Fmwin32_handmade.map /link %CommonLinkerFlags%
+cl %CommonCompilerFlags% %~dp0win32_handmade.cpp -Fmwin32_handmade.map /link %CommonLinkerFlags%
 popd
